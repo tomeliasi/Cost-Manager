1de061f971e41ed680c39bc12824118a78ec24b0
tomeliasi <tomeliasi65@gmail.com>
2026-01-14 18:51:11 +0200

    add(vite.config.js) : add allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  server: { port: 5173 },
+
+  preview: {
+    allowedHosts: ['.onrender.com']
+  }
 });
